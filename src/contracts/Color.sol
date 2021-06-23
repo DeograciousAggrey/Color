6830bf8fd2bcc50b109c13f9a8ce9d281e99d8c8
DeograciousAggrey <DeograciousAggrey@gmail.com>
2021-06-23 15:58:21 +0300

    Updated code files

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -1,11 +1,28 @@
-//SPDX-Licence-Identifier: GPL-3.0
-pragma solidity >=0.5.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.5.0 < 0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Color is ERC721 {
-    constructor () ERC721 ("Color","COLOR") public {
 
+    string[] public colors;
+    mapping(string => bool) colorExists;
+
+    constructor () ERC721 ("Color","COLOR") {
+
+    }
+
+    function mint(string memory _color) public {
+        require(!colorExists[_color]);
+         for(uint i = 0; i<colors.length; i++){
+             
+                 colors.push(_color);
+                 uint id = colors.length - 1;
+                 _mint(msg.sender, id);
+                 colorExists[_color] = true;
+             
+         }
+         
     }
     
 }
--- test/Color.test.js
+++ test/Color.test.js
@@ -1,10 +1,38 @@
+const { assert } = require("chai");
+
 const Color = artifacts.require("Color");
 
-contract ('Color' (accounts) => {
+contract ('Color',(accounts) => {
+    let contract;  
+
+    before (async () =>{
+        contract = await Color.deployed();
+    });
 
     describe('deployment', async () => {
         it('deploys succesfully', async () => {
-            
+           
+            const address = contract.address;
+            assert.notEqual(address, '');    
+        });
+
+        it ('has a name', async () => {
+            const name = await contract.name();
+            assert.equal(name, 'Color', "The name of the contract should be correct");
+        });
+
+        it('has a symbol', async () => {
+            const symbol = await contract.symbol();
+            assert.equal(symbol,'COLOR', "Symbol should be correct");
+        });
+
+    });
+
+    describe('minting a new token', async() => {
+        it('creates a new token', async() => {
+            const result = await contract.mint('#E8HUE9');
+            //SUCCESS
+            console.log(result);
         });
     });
 
