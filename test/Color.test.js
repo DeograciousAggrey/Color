5b8947c19dd3d97ee8095ffa7206a82d5fbeeb9b
DeograciousAggrey <DeograciousAggrey@gmail.com>
2021-06-24 22:33:14 +0300

    Updated the test files

--- test/Color.test.js
+++ test/Color.test.js
@@ -1,4 +1,7 @@
 const { assert } = require("chai");
+const { showCompletionScript } = require("yargs");
+
+require('chai').use(require('chai-as-promised')).should();
 
 const Color = artifacts.require("Color");
 
@@ -32,8 +35,46 @@ contract ('Color',(accounts) => {
         it('creates a new token', async() => {
             const result = await contract.mint('#E8HUE9');
             //SUCCESS
-            console.log(result);
+            //console.log(result);
+            const event = result.logs[0].args;
+            assert.equal(event.tokenId,1,"Id should be correct");
+
+            //FAILURE
+            await contract.mint('#E8HUE9').should.be.rejected;
+
         });
     });
+
+    describe('indexing', async() => {
+        it('lists colors', async() => {
+            //Mint 3 new tokens
+            await contract.mint('#TH8DJ5');
+            await contract.mint('#DVN347');
+            await contract.mint('#000000');
+            let Supply = await contract.totalsupply;
+            console.log(Supply);
+
+            let color ;
+            let result = [];
+    
+           /* for(var i = 0; i <= totalSupply; i++) {
+                color = await contract.colors[i-1];
+                result.push(color);
+            };
+            */
+           // let expectedResult = ['#E8HUE9','#TH8DJ5','#DVN347','#000000'];
+           // assert.equal(result.join(','),expectedResult.join(','), " Total Supply Should be correct");
+        });
+    });
+    
+   describe ('Total Supply', async() => {
+       it('Shows the total supply', async()=> {
+           let expectedTotal = 1;
+           let supply = await contract.totalsupply;
+          console.log(supply);
+           // assert.equal(expectedTotal, result, 'The total supply should be correct');
+       });
+   });
+    
 
 });
